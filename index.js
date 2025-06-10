19bc90c9513593c20c13d48919c688052d691724
Edward Karogo <prezokams@gmail.com>
2025-06-10 08:04:31 +0300

    JS control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  if (distance <= 400) {
+    let returnMsg = `This one is on me!`;
+    return returnMsg;
+  } else if (distance < 2000) {
+    let returnMsg = `That will be twenty bucks.`;
+    return returnMsg;
+  } else if (distance <= 2500) {
+    let returnMsg = `I will gladly take your thirty bucks.`;
+    return returnMsg;
+  } else if (distance > 2500) {
+    let returnMsg = `No can do.`;
+    return returnMsg;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(myCity) {
   // Write your code here!
+  let str;
+  str = myCity == 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return str;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipFee) {
   // Write your code here!
+  let msg;
+  switch (tipFee) {
+    case "generous":
+      msg = "Thank you so much.";
+      return msg;
+      break;
+    case "not as generous":
+      msg = "Thank you.";
+      return msg;
+      break;
+    default:
+      msg = "Bye.";
+      return msg;
+  }
 }
